dfaa6d78329e4b0f0c068eefb73f359a02a33382
JoseHatake <di_tutticolori@hotmail.com>
2016-09-21 23:46:58 -0500

    Estructura principal

---
+++ WEB-INF/src/mx/escom/ipn/wad/servlets/LeerParametros.java
@@ -0,0 +1,32 @@
+package mx.escom.ipn.wad.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+//@WebServlet(name = "LeerParametros", urlPatterns = {"/LeerParametros"})
+public class LeerParametros extends HttpServlet {
+    private Integer counter;
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        request.getRequestDispatcher("/index.html").forward(request, response);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+}
